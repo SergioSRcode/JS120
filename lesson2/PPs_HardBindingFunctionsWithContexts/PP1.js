2df040d3c2a76b11432bc618804f587efc06c0b4
Sergio Ravera <sergioravera@hotmail.de>
2024-06-27 15:15:11 +0200

    added PP1

---
+++ lesson2/PPs_HardBindingFunctionsWithContexts/PP1.js
@@ -0,0 +1,5 @@
+// What method can we use to bind a
+// function permanently to a particular execution context?
+
+//=> bind, which returns a function with its execution context permanently
+// altered according its first argument.
