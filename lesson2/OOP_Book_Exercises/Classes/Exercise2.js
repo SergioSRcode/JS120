d0dd2f1ae74377601b1377e0d909c83d7d53f16d
Sergio Ravera <sergioravera@hotmail.de>
2024-07-06 16:03:38 +0200

    added Exercise2

---
+++ lesson2/OOP_Book_Exercises/Classes/Exercise2.js
@@ -0,0 +1,4 @@
+// If you have a Dog class and an object assigned to a variable named boo,
+// how can you tell whether the object is an instance of the Dog class?
+
+console.log(boo instanceof Dog);
