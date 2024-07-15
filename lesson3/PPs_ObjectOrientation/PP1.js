a490b1f8dd2bb23d0913d26d8976b5a0bd6984d6
Sergio Ravera <sergioravera@hotmail.de>
2024-07-15 14:45:42 +0200

    added PP1

---
+++ lesson3/PPs_ObjectOrientation/PP1.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+// Suppose we want to use code to keep track of products in our hardware store's inventory.
+// A first stab might look something like this:
+
+// let scissorsId = 0;
+// let scissorsName = 'Scissors';
+// let scissorsStock = 8;
+// let scissorsPrice = 10;
+
+// let drillId = 1;
+// let drillName = 'Cordless Drill';
+// let drillStock = 15;
+// let drillPrice = 45;
+
+
+let scissors = {
+  id: 0,
+  name: "Scissors",
+  stock: 8,
+  price: 10,
+};
+
+let drill = {
+  id: 1,
+  name: "Cordless Drill",
+  stock: 15,
+  price: 45,
+};
