3fda6237c8db6dcfb24b790ddc5b216dbb582a40
Sergio Ravera <sergioravera@hotmail.de>
2024-07-17 16:08:20 +0200

    added PP4 and PP5

--- lesson3/PPs_ConstructorsAndPrototypes/PP4.js
+++ lesson3/PPs_ConstructorsAndPrototypes/PP4.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line max-len
-// What will the following code output and why? Try to answer without running the code.
+// What will the following code log to the console and why?
 
 function Ninja() {
   this.swung = true;
@@ -7,20 +6,15 @@ function Ninja() {
 
 let ninja = new Ninja();
 
-Ninja.prototype = {
-  swingSword: function() {
-    return this.swung;
-  },
+Ninja.prototype.swingSword = function() {
+  return this.swung;
 };
 
 console.log(ninja.swingSword());
 
 /*
-line 10 reassigns the prototype property of the Ninja constructor to
-an anonymous object containing the swingSword method. Since the
-ninja object was instantiated before the reassignment, it still has the
-previous prototype of Ninja. As there was no change in prototype for the
-object ninja, the swingSword method doesn't exist and throws an error.
-
-
+On line 9, a swingSword method is declared on the function prototype of the
+Ninja constructor function. Since the instantiated ninja object has the
+function/object prototype of the Ninja constructor as its own prototype,
+line 13 loggs true to the console.
 */
