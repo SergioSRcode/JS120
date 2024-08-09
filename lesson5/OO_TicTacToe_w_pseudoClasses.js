d23632c93fbd79db390849350ec446aed8269529
Sergio Ravera <sergioravera@hotmail.de>
2024-08-09 11:48:46 +0200

    implemented the Board pseudo-class

--- lesson5/OO_TicTacToe_w_pseudoClasses.js
+++ lesson5/OO_TicTacToe_w_pseudoClasses.js
@@ -26,3 +26,60 @@ Square.prototype.getMarker = function() {
 
 // let square = new Square(Square.COMPUTER_MARKER);
 // console.log(square.marker);
+
+
+function Board() {
+  this.squares = {};
+
+  for (let counter = 1; counter <= 9; ++counter) {
+    this.squares[counter] = new Square();
+  }
+}
+
+Board.prototype.display = function() {
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${this.squares["1"]}  |  ${this.squares["2"]}  |  ${this.squares["3"]}  `);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${this.squares["4"]}  |  ${this.squares["5"]}  |  ${this.squares["6"]}  `);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}  `);
+  console.log("     |     |");
+  console.log("");
+};
+
+Board.prototype.displayWithClear = function() {
+  console.clear();
+  console.log("");
+  console.log("");
+  this.display();
+};
+
+Board.prototype.markSquareAt = function(key, marker) {
+  this.squares[key].setMarker(marker);
+};
+
+Board.prototype.unusedSquares = function() {
+  let keys = Object.keys(this.squares);
+  return keys.filter(key => this.squares[key].isUnused());
+};
+
+Board.prototype.isFull = function() {
+  return this.unusedSquares().length === 0;
+};
+
+Board.prototype.countMarkersFor = function(player, keys) {
+  let markers = keys.filter(key => {
+    return this.squares[key].getMarker() === player.getMarker();
+  });
+
+  return markers.length;
+};
+
+// let board = new Board();
+// board.markSquareAt(6, Square.COMPUTER_MARKER);
+// board.display();
