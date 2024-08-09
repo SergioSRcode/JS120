b07987b59fa5a9bb94c9dc7c46f1ef0a811d92b0
Sergio Ravera <sergioravera@hotmail.de>
2024-08-09 12:08:07 +0200

    finished working solution for the pseudoclassical OO TTTgame

--- lesson5/OO_TicTacToe_w_pseudoClasses.js
+++ lesson5/OO_TicTacToe_w_pseudoClasses.js
@@ -110,3 +110,106 @@ Computer.prototype.constructor = Computer;
 // console.log(human.getMarker());
 // let computer = new Computer();
 // console.log(computer.getMarker());
+
+function TTTGame() {
+  this.board = new Board();
+  this.human = new Human();
+  this.computer = new Computer();
+}
+
+TTTGame.POSSIBLE_WINNING_ROWS = [
+  [ "1", "2", "3" ],            // top row of board
+  [ "4", "5", "6" ],            // center row of board
+  [ "7", "8", "9" ],            // bottom row of board
+  [ "1", "4", "7" ],            // left column of board
+  [ "2", "5", "8" ],            // middle column of board
+  [ "3", "6", "9" ],            // right column of board
+  [ "1", "5", "9" ],            // diagonal: top-left to bottom-right
+  [ "3", "5", "7" ],            // diagonal: bottom-left to top-right
+];
+
+TTTGame.prototype.play = function() {
+  this.displayWelcomeMessage();
+  this.board.display();
+
+  while (true) {
+    this.humanMoves();
+    if (this.gameOver()) break;
+
+    this.computerMoves();
+    if (this.gameOver()) break;
+
+    this.board.displayWithClear();
+  }
+  this.board.displayWithClear();
+  this.displayResults();
+  this.displayGoodbyeMessage();
+};
+
+TTTGame.prototype.displayWelcomeMessage = function() {
+  console.clear();
+  console.log("Welcome to Tic Tac Toe!");
+  console.log("");
+};
+
+TTTGame.prototype.displayGoodbyeMessage = function() {
+  console.log("Thanks for playing Tic Tac Toe! Goodbye!");
+};
+
+TTTGame.prototype.displayResults = function() {
+  console.clear();
+  this.board.display();
+
+  if (this.isWinner(this.human)) {
+    console.log("You won! Congatulations!");
+  } else if (this.isWinner(this.computer)) {
+    console.log("I won! I won! Take that, human! The revolution is upon us!");
+  } else {
+    console.log("A tie. Next time I will wear a sweater!");
+  }
+};
+
+TTTGame.prototype.isWinner = function(player) {
+  return TTTGame.POSSIBLE_WINNING_ROWS.some(row => {
+    return this.board.countMarkersFor(player, row) === 3;
+  });
+};
+
+TTTGame.prototype.humanMoves = function() {
+  let choice;
+
+  while (true) {
+    let validChoices = this.board.unusedSquares();
+    const prompt = `Choose a square(${validChoices.join(", ")}): `;
+    choice = readline.question(prompt);
+
+    if (validChoices.includes(choice)) break;
+
+    console.log("Sorry, that's not a valid choice.");
+    console.log("");
+  }
+
+  this.board.markSquareAt(choice, this.human.getMarker());
+};
+
+TTTGame.prototype.computerMoves = function() {
+  let validChoices = this.board.unusedSquares();
+  let choice;
+
+  do {
+    choice = String(Math.floor((9 * Math.random()) + 1));
+  } while (!validChoices.includes(choice));
+
+  this.board.markSquareAt(choice, this.computer.getMarker());
+};
+
+TTTGame.prototype.gameOver = function() {
+  return this.board.isFull() || this.someoneWon();
+};
+
+TTTGame.prototype.someoneWon = function() {
+  return this.isWinner(this.human) || this.isWinner(this.computer);
+};
+
+let game = new TTTGame();
+game.play();
